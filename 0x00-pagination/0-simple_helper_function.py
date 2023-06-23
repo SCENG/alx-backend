86788e6e656437dbfddee0baf7ce2a6ba5541154
Elijah Sinkamba <scengj@gmail.com>
2023-06-23 09:44:02 +0200

    tuple insertion

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -13,5 +13,6 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     Returns:
         (tuple): a tuple of the start and end index for the given page
     """
-    nextPageStartIndex = page * page_size
-    return nextPageStartIndex - page_size, nextPageStartIndex
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
